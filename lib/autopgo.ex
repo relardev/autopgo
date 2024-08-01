3013e136fd00f178df976ab7c26f3df596fbc61b
relar <relardev@gmail.com>
2024-08-01 20:19:14 +0200

    batman

---
+++ lib/autopgo.ex
@@ -0,0 +1,18 @@
+defmodule Autopgo do
+  @moduledoc """
+  Documentation for `Autopgo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Autopgo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/autopgo_test.exs
@@ -0,0 +1,8 @@
+defmodule AutopgoTest do
+  use ExUnit.Case
+  doctest Autopgo
+
+  test "greets the world" do
+    assert Autopgo.hello() == :world
+  end
+end
